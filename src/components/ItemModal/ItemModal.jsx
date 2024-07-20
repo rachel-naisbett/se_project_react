f02657b7cba9fa969a1414ede111fa34dbfda2d9
Rachel N <software.engineer.rachel@gmail.com>
2024-07-20 12:15:54 -0400

    fixed key issue

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,15 +1,17 @@
 import "./ItemModal.css";
-import closeBTN from "../../assets/closeBTN.svg";
 function ItemModal({ activeModal, card, handleCloseClick }) {
   return (
-    <div className={`popup ${activeModal === "preview" && "popup_opened"}`}>
+    <div
+      key={card}
+      className={`popup ${activeModal === "preview" && "popup_opened"}`}
+    >
       <div className="popup__content popup__content-type-image">
         <button
           onClick={handleCloseClick}
           type="button"
           className="popup__close"
         ></button>
-        <img src={card.link} alt="" className="popup__image" />
+        <img src={card.link} alt={card.name} className="popup__image" />
         <div className="popup__footer-item">
           <h2 className="popup__caption">{card.name}</h2>
           <p className="popup__weather">Weather: {card.weather}</p>
